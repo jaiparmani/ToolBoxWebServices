385ce550e8e5d7344ce9ca0fbffd4db2602650ce
Jai Parmani <jaiparmani35@gmail.com>
2025-11-07 23:58:09 +0530

    fix

--- toolboxweb/toolboxweb/settings.py
+++ toolboxweb/toolboxweb/settings.py
@@ -146,7 +146,6 @@ REST_FRAMEWORK = {
     },
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ],
 }
 
